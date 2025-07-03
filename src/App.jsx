d1bd0569648a826596509394c86b08a9ec9e6edf
Harshada Palanikkumar <hpalanik@ucsc.edu>
2025-07-03 12:23:13 -0700

    add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 
+const imageImport = import.meta.glob('./assets/*.png', {eager: true, as: 'url'});
+const images = Object.values(imageImports);
+
 function App() {
   return (
     <div>
 
       <div className="headerRow">
-
         <h1 className="header">Barbie Guess Who</h1>
         <button className="reset">Reset</button>
-
       </div>
 
     </div>
