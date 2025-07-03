aafb95c11670ec0ded4edb1ac3e56e098c161ded
Harshada Palanikkumar <hpalanik@ucsc.edu>
2025-07-02 18:27:30 -0700

    Initial Hello World setup with React and Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>Hello World 👋</h1>
+      <p>Guess Who starter project is running!</p>
+    </div>
+  );
+}
+
+export default App;
