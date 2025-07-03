aa7870e2ba4a05e44d2e926a0175e9e189b54c86
Harshada Palanikkumar <hpalanik@ucsc.edu>
2025-07-03 12:42:11 -0700

    load images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
 
-const imageImport = import.meta.glob('./assets/*.png', {eager: true, as: 'url'});
+// import all images at the same time using an array
+//use vite's import.meta.glob to grab all the assets
+const imageImports = import.meta.glob('./assets/*.jpg', { eager: true, as: 'url' });
 const images = Object.values(imageImports);
+
 
 function App() {
   return (
@@ -10,6 +13,18 @@ function App() {
       <div className="headerRow">
         <h1 className="header">Barbie Guess Who</h1>
         <button className="reset">Reset</button>
+      </div>
+
+     
+      <div className="images">
+        {images.map((src, index) => (  // for loop to load all images
+          <img
+            key={index}
+            src={src}
+            alt={`character-${index + 1}`}
+            className="character-card" 
+          />
+        ))}
       </div>
 
     </div>
